91313449253df05752b80d4b6fb6e48c262d6efd
czw <nick.cui@keeson.com>
2023-02-11 16:19:06 +0800

    add save state toast

--- ZWQRCode/ZWQRCodeViewController.m
+++ ZWQRCode/ZWQRCodeViewController.m
@@ -44,7 +44,29 @@
 
 - (void)saveQRCodeImage {
     // 保存图像,需要提前在info.plist文件中写入访问相册的权限,否则会报错
-    UIImageWriteToSavedPhotosAlbum(_QRCodeView.QRCodeImage.image, nil, nil, nil);
+//    UIImageWriteToSavedPhotosAlbum(_QRCodeView.QRCodeImage.image, nil, nil, nil);
+    UIImageWriteToSavedPhotosAlbum(_QRCodeView.QRCodeImage.image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+}
+
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo {
+    NSString *title;
+    NSString *message;
+    
+    if (!error) {
+        title = @"Success!";
+        message = @"The QRCode image saved successfully.";
+    }
+    else {
+        title = @"Failed!";
+        message = @"The QRCode image saved unsuccessfully, please try again later.";
+    }
+    
+    // 使用alert view显示二维码保存状态
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+    [alert addAction:action];
+    
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 @end
